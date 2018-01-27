85771ae19f8abcd351958f3ceba74dc0f8bb770d
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-01-27 13:37:30 -0500

    allow passing characters to SVG

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000
 const bullet = require('./bullet')
 
 app.get('/bullet', (req, res) => {
-  res.send(bullet())
+  res.send(bullet(req.query.letter))
 })
 
 console.log('Server is running on port', port)
