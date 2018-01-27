ee453499a5e88c7d12f475ee5b2b38fb1a485ba9
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-01-27 16:45:45 -0500

    get rid of this error handling for now

--- stations.js
+++ stations.js
@@ -1,9 +1,7 @@
+const stationdata = require('./stationdata.json')
+const stations = stationdata.data
+
 function randomStation(){
-  return new Promise((resolve, reject) => {
-    const stationdata = require('./stationdata.json')
-    const stations = stationdata.data
-    let station = stations[Math.floor(Math.random()*stations.length)][10]
-    resolve(station)
-  })
+  return stations[Math.floor(Math.random()*stations.length)][10]
 }
 module.exports = randomStation
