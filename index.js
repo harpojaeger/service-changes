06db70502b18770f61afd34b65ce68f9beb039de
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-01-27 14:15:38 -0500

    pass along the bulletcolor and textcolor query params too

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000
 const bullet = require('./bullet')
 
 app.get('/bullet', (req, res) => {
-  res.send(bullet(req.query.alphanum))
+  res.send(bullet(req.query.alphanum, req.query.bulletcolor, req.query.textcolor))
 })
 
 console.log('Server is running on port', port)
