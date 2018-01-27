e2e8f2e0fd637764bb6260a274e0e11936b8564e
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-01-27 14:53:01 -0500

    /station endpoint to return random station name

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const app = require('express')()
 require ('dotenv').config()
 const port = process.env.PORT || 5000
 const bullet = require('./bullet')
+const station = require('./stations')
 
 app.get('/bullet', (req, res) => {
   res.send(bullet(req.query.alphanum, req.query.bulletcolor, req.query.textcolor))
+})
+
+app.get('/station', (req, res) => {
+  res.send(station())
 })
 
 console.log('Server is running on port', port)
---
+++ stations.js
@@ -0,0 +1,6 @@
+const stationdata = require('./stationdata.json')
+const stations = stationdata.data
+function randomStation(){
+  return stations[Math.floor(Math.random()*stations.length)][10]
+}
+module.exports = randomStation
