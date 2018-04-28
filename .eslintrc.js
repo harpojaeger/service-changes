7d616f0a123761724641731a4c0812f1ca843c43
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-04-28 13:49:39 -0400

    adjust quote rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,8 @@ module.exports = {
         ],
         "quotes": [
             "error",
-            "single"
+            "single",
+            {"avoidEscape": true}
         ],
         "semi": [
             "error",
