8fdae078dfa450d03042e184eb9e08730ba11906
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-04-30 09:36:00 -0400

    forgot a new file

---
+++ components/directions.js
@@ -0,0 +1,24 @@
+const compassDirections = [
+  'north',
+  'south',
+  'east',
+  'west',
+  'northeast',
+  'southeast',
+  'southwest',
+  'northweste',
+  'north-northeast',
+  'east-northeast',
+  'east-southeast',
+  'south-southeast',
+  'south-southwest',
+  'west-southwest',
+  'west-northwest',
+  'north-northwest'
+]
+
+export function compassDirection({cap = false}) {
+  var compassDirection = compassDirections[Math.floor(Math.random()*compassDirections.length)]
+  if (cap) compassDirection = `${compassDirection.charAt(0).toUpperCase()}${compassDirection.slice(1)}`
+  return compassDirection
+}
