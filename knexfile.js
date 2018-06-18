e38389519bd4e3029e4fc26ba45d55fa5abb91a9
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-06-18 14:27:01 -0400

    clean up envs

--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,18 @@
 require ('dotenv').config()
-const {
-  PROD_DATABASE_URL,
-  STAGE_DATABASE_URL,
-  DEV_DATABASE_URL
-} = process.env
+const {DATABASE_URL} = process.env
 
 // dev defaults to sqlite3
-const development = DEV_DATABASE_URL ? {
+const development = DATABASE_URL ? {
   client: 'postgresql',
-  connection: DEV_DATABASE_URL,
+  connection: DATABASE_URL,
 } : {
   client: 'sqlite3',
   connection: {filename: './dev.sqlite3'},
   useNullAsDefault: true
 }
-const production = {
+const pg = {
   client: 'postgresql',
-  connection: PROD_DATABASE_URL
+  connection: DATABASE_URL
 }
-// staging inherits production settings if not set explicitly
-const staging = STAGE_DATABASE_URL ? {
-  client: 'postgresql',
-  connection: STAGE_DATABASE_URL
-} : Object.assign({}, production)
 
-module.exports = {development, production, staging}
+module.exports = {development, production: pg, staging: pg}
